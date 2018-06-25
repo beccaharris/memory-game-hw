5233dc001d064fd96f6afa4bdab5f88d80d1b260
Becca Harris <beccalynharris@gmail.com>
2018-06-25 01:20:20 -0600

    fix blank space after footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css';
 const Footer = props => {
   return (
     <footer>
-      <h2>&copy; 2018 Becca Harris <br/><a className="github-link" href="https://github.com/beccaharris/memory-game-hw" target="_blank"><i class="fab fa-github-alt"></i></a></h2>
+      <h2 class="footer-h2">&copy; 2018 Becca Harris <br/><a className="github-link" href="https://github.com/beccaharris/memory-game-hw" target="_blank"><i class="fab fa-github-alt"></i></a></h2>
       
     </footer>
   )
